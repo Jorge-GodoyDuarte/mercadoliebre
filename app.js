93e9149cc8f67d4d9892b373c57c39f40205ca65
Jorge-GodoyDuarte <hawkaa666@gmail.com>
2022-07-03 19:15:19 -0300

    agregue app-static y se ven las imagenes!

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const port ="3030";
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
-
+app.use(express.static('public'));
 app.listen(port,()=>console.log("servidor corriendo en el  puerto 3030"));
